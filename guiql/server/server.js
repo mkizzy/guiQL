7cc993cec0e48b801973a5a92c2fa5fe5ff37c24
Marco Kwan <mrcokwn@gmail.com>
2023-02-05 23:20:47 -0500

    first commit

---
+++ guiql/server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const mongoose = require('mongoose');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Unknown route handler
+app.use((req, res) => res.sendStatus(404));
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
