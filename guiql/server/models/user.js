7750cfe1b3e8fe8e168f1b2cec89593b68c7b331
Marco Kwan <mrcokwn@gmail.com>
2023-02-27 13:09:18 -0500

    clusterinitialized

--- guiql/server/models/user.js
+++ guiql/server/models/user.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'guiQL'
+  })
+    .then(() => console.log('Successfully connected to Mongo DB.'))
+    .catch(err => console.log(err));
+  //create schema
 const userSchema = new mongoose.Schema({
     firstname: {type: String},
     lastname: {type: String},
