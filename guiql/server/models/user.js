c9db1b80c0b223225e53546c8cf815bc654d9b38
Marco Kwan <mrcokwn@gmail.com>
2023-02-27 23:45:27 -0500

    fixedserverconnection

--- guiql/server/models/user.js
+++ guiql/server/models/user.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: 'guiQL'
-  })
-    .then(() => console.log('Successfully connected to Mongo DB.'))
-    .catch(err => console.log(err));
-  //create schema
 const userSchema = new mongoose.Schema({
     firstname: {type: String},
     lastname: {type: String},
@@ -16,5 +8,4 @@ const userSchema = new mongoose.Schema({
     password: {type: String},
     savedURI: [{type: String, ref: 'User'}]
 });
-
 module.exports = mongoose.model("User", userSchema)
--- guiql/server/server.js
+++ guiql/server/server.js
@@ -1,14 +1,30 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-// Unknown route handler
-app.use((req, res) => res.sendStatus(404));
+const URI = process.env.MONGO_URI;
+mongoose.set("strictQuery", false);
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'guiQL'
+})
+.then(()=>{
+  app.listen(PORT, ()=>{
+      console.log(`Server is on ${PORT}`)
+  });
+})
+.catch(err=>{
+  console.log('db connection failed. Server not start.');
+  console.error(err);
+})
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -22,4 +38,6 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+// Unknown route handler
+app.use((req, res) => res.sendStatus(404));
+module.exports = app
