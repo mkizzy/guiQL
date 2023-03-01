9402d3189b7e7a24f4a335241207d173112c907c
Tsering Tsering <dtseringt2@gmail.com>
2023-03-01 14:15:23 -0500

    login modal completed without functionality so far

--- guiql/src/App.tsx
+++ guiql/src/App.tsx
@@ -42,6 +42,7 @@ function App() {
   }
   return (
     <div className="app bg-amber-50">
+      
       <Navbar 
         isTopOfPage = {isTopOfPage}
         selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}
--- guiql/src/scenes/credentialModal/index.tsx
+++ guiql/src/scenes/credentialModal/index.tsx
@@ -5,15 +5,11 @@ type Props = {}
 
 const CredentialModal = (props: Props) => {
     return (
-        <div className = "relative z-10">
-            <div className = "fixed inset-0 bg-black bg-opacity-30 z-10">
-                <div className="fixed inset-0 z-10 overflow-y-auto">
-                    <div className='flex min-h-full items-center justify-center p-4 text-center z-10'>
+                <div className='fixed inset-0 z-10 overflow-y-auto'>
+                    <div className="flex min-h-full items-center justify-center p-4 text-center z-10">
                         <LoginModal/>
                     </div>
                 </div>
-            </div>
-        </div>
     )
 }
 
