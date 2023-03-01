55b510c7bda36b83ddcbe8a9721ac627bc1a2b15
Tsering Tsering <dtseringt2@gmail.com>
2023-03-01 14:41:13 -0500

    added comments for next next; need a lot of refactoring for the forms

--- guiql/src/scenes/credentialModal/LoginModal.tsx
+++ guiql/src/scenes/credentialModal/LoginModal.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const LoginModal = () => {
-    return (
+type Props = {
 
+}
+const LoginModal = (props: Props) => {
+    return (
         <div className="fixed inset-0 flex items-center">
             <div className="fixed inset-0 bg-black opacity-75 z-10"></div>
-            <div className="relative mx-6 md:mx-auto w-full md:w-1/2 lg:w-1/3 z-20 m-8">
+            <div className="relative mx-6 md:mx-auto w-full md:w-1/4 z-20 m-8">
                 <div className="shadow-lg bg-white rounded-lg p-8">
                     <div className="flex justify-end mb-6">
                         <button>
@@ -13,35 +15,38 @@ const LoginModal = () => {
                         </button>
                     </div>
                     <h1 className="text-center text-2xl">Login</h1>
-                    <form className="pt-6 pb-2 my-2 ">
-                    <div className="mb-4 text-left">
-                        <label className="block text-sm font-bold mb-2">Email Address</label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                            id="email"
-                            type="text"
-                            placeholder="Email Address"
-                        />
-                    </div>
-                    <div className="mb-6 text-left">
-                        <label className="block text-sm font-bold mb-2">Password</label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3"
-                            id="password"
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div className="block md:flex items-center justify-between">
-                        <div>
-                        <button className="rounded-md hover:bg-secondary-500 px-10 py-2 bg-primary-500 text-white" type="button">
-                            Sign In
-                        </button>
+                    <form className="pt-6 pb-2 my-2">
+                        {/* Need states to track input values*/}
+                        <div className="mb-4 text-left">
+                            <label className="block text-sm font-bold mb-2">Email Address</label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3"
+                                id="email"
+                                type="text"
+                                placeholder="Email Address"
+                            />
                         </div>
-                        <div className="mt-4 md:mt-0">
-                            <a href="#" className="text-green no-underline">Forget Password?</a>
+                        <div className="mb-6 text-left">
+                            <label className="block text-sm font-bold mb-2">Password</label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 mb-3"
+                                id="password"
+                                type="password"
+                                placeholder="Password"
+                            />
                         </div>
-                    </div>
+                        <div className="block md:flex items-center justify-between">
+                            <div>
+                                {/* Add functinoality here */}
+                                <button className="rounded-md hover:bg-secondary-500 px-10 py-2 bg-primary-500 text-white" type="button">
+                                    Sign In
+                                </button>
+                            </div>
+                            <div className="mt-4 md:mt-0">
+                                {/* Add functinoality here */}
+                                <a href="#" className="no-underline">Forget Password?</a> 
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
--- guiql/src/scenes/credentialModal/SignUpModal.tsx
+++ guiql/src/scenes/credentialModal/SignUpModal.tsx
@@ -5,14 +5,7 @@ type Props = {}
 const SignUpModal = (props: Props) => {
     return (
         <>
-        <h1>Create An Account</h1>
-        <form>
-            <label>Email Address</label>
-            <input type="text" placeholder = "Email Address"/>
-            <label>Password</label>
-            <input type = "password" placeholder = "Password" />
-            <button>Sign Up</button>
-        </form>
+
         </>
     )
 }
