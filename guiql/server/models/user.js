a349e9077ed2a481fefa0e0a281e1c293563131a
Tsering Tsering <dtseringt2@gmail.com>
2023-03-02 14:56:53 -0500

    controllers and routes being set up

--- guiql/server/models/user.js
+++ guiql/server/models/user.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const userSchema = new mongoose.Schema({
-    firstname: {type: String},
-    lastname: {type: String},
+    fullName: {type:String},
     email: {type:String, unique: true},
     username: {type: String, unique: true},
     password: {type: String},
