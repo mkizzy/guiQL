4dd093baa9d6ef7f287d891fa918976772352686
Tsering Tsering <dtseringt2@gmail.com>
2023-03-02 15:32:40 -0500

    postRegister controller completed

--- guiql/server/controllers/postRegister.js
+++ guiql/server/controllers/postRegister.js
@@ -1,5 +1,52 @@
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
 const postRegister = async (req, res) => {
-    res.send("register route")
+    try {
+        const {firstName, lastName, email, password, confirmPassword} = req.body;
+        
+        //check if user exist
+        const userExists = await User.exists({email});
+        if(userExists){
+            return res.status(409).send('Email has already been registered.');
+        }
+        
+        //encrypting password
+        const encryptedPassword = await bcrypt.has(password, 10);
+        
+        //create a new user account in the db
+        const user = await User.create({
+            firstName,
+            lastName,
+            email: email.toLowerCase(),
+            password: encryptedPassword
+        })
+        
+        //creating a token
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                email
+            },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn:'24h'
+            }
+        )
+        
+        //sending info to client upon successful request
+        res.status(201).json({
+            userDetails:{
+                email: user.email,
+                token: token,
+                firstName: user.firstName,
+                lastName: user.lastName
+            },
+        });
+    } catch (err) {
+        return res.status(500).send('Error has occured. Please try again.');
+    }
 }
 
-module.exports = postRegister
+module.exports = postRegister;
--- guiql/server/models/user.js
+++ guiql/server/models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const userSchema = new mongoose.Schema({
-    fullname: {type:String},
+    firstName: {type:String},
+    lastName: {type:String},
     email: {type:String, unique: true},
     password: {type: String},
 });
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("user", userSchema)
