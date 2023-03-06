a6e6781547a028b4448618bb6958c20c769a2351
Marco Kwan <mrcokwn@gmail.com>
2023-03-06 13:50:28 -0500

    tests in progress

--- __tests__/postRegisterTest.js
+++ __tests__/postRegisterTest.js
@@ -15,45 +15,60 @@ chai.use(chaiHttp);
 //test login functionality, test for proper status code, expect content type to be json object, req.body to be object
 //not sure if done should be included, its for testing callbacks
 //not 100% on how to test JWT tokens tbh
-describe('post register', () => {
+describe('post register API function', () => {
     const newUser = {
         firstName: 'Kevin',
         lastName: 'Lee',
         email: 'kevin.lee@gmail.com',
         password: 'KevinLee1!',
-        // confirmPassword: 'KevinLee1!'
+        confirmPassword: 'KevinLee1!'
     }
 
-    let token;
-
-    // register
-    beforeEach(() => {
+    it('should register the user, login user, check for JWT token, user will be removed after', () => {
         chai
             .request(app)
-            .post("/register")
+            .post("/auth/register")
             .send(newUser)
             .end((err, res) => {
-                token = req.body.userDetails.token;
-                res.should.have.status(201);
+                // should receive 201 status with object
+                res.should.have.status(201)
+
+                // process login
+                chai.request(app)
+                    .post('/auth/register')
+                    //send in user login data
+                    .send({
+                        'firstName': 'tester',
+                        
+                    })
+
+                // token = req.body.userDetails.token;
+                // res.should.have.status(201);
             });
     });
+    // afterEach(() => {
+    //     // remove the registered user
+    // })
+    
+    // let token;
 
     // sample test to check if jest works
     it('should return -1 when the value is not present', function () {
         assert.equal([1, 2, 3].indexOf(4), -1);
     });
 
-    // it('should create a token, check for valid credentials, and return JSON obj of info', (done) => {
-    //     chai
-    //         .request(app)
-    //         .get('/register')
-    //         .set({ userId: `Bearer ${token}` })
-    //         .end((err, res) => {
-    //             expect(res.statusCode).to.equal(201);
-    //             expect(res.body).to.be.an('object');
-    //             expect(res.body).should.have.property('userDetails');
-    //             expect(token).to.have.property('userId');
-    //             done();
-    //         })
-    // })
+    it('should throw error for invalid JWT', (done) => {
+        chai
+            .request(app)
+            .get('/auth/register')
+            .end((err, res) => {
+                
+            })
+    })
 });
+//check for valid credentials, and return JSON obj of info'
+// expect(res.statusCode).to.equal(201);
+// expect(res.body).to.be.an('object');
+// expect(res.body).should.have.property('userDetails');
+// expect(token).to.have.property('userId');
+// done();
